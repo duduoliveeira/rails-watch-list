4e30f0dc193e58d876e75289a53f37a646fded09
Eduardo Oliveira <edu.oliveeira@hotmail.com>
2022-11-18 15:22:13 -0300

    Models and controllers ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :lists
+  root to: 'lists#index'
+
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
